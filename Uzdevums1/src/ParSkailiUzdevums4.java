b48afa7535b2d1814b1c41e9f29f4f066f0e2ec0
peteris.troksa <peteris.troksa@inbox.lv>
2023-03-27 20:40:16 +0300

    Otrs uzdevums?

---
+++ Uzdevums1/src/ParSkailiUzdevums4.java
@@ -0,0 +1,20 @@
+public class ParSkailiUzdevums4 {
+    public static void main(String[] args) {
+        int skaitlis = 1;
+        if (skaitlis < 0) {
+            System.out.println("Skaitlis ir negativs");
+        }
+        else if (skaitlis>0){
+            System.out.println("Skaitlis ir pozitivs");
+        }
+        else if (skaitlis==0){
+            System.out.println("Skaitlis ir vienads ar nulli");
+        }
+
+
+
+
+
+    }
+}
+//== ir vienads ar !=nav vienads ar >+ ir lielaks vai vienads
