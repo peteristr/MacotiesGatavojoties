187f461d93cf9e9ab8f85f945867f502476528e8
peteris.troksa <peteris.troksa@inbox.lv>
2023-03-27 13:40:47 +0300

    FirstComit-PT

---
+++ Uzdevums1/src/BankasDati.java
@@ -0,0 +1,10 @@
+public class BankasDati {
+    public String bankName = "Citadele Banka";
+    public String bankName2;
+    public String paymentProcessor = "Master Card";
+    public long cardNumber = 655656556;
+    public int secirityNumber = 111;
+
+
+
+}
---
+++ Uzdevums1/src/Funkcija.java
@@ -0,0 +1,15 @@
+public class Funkcija {
+
+    public static void main(String[] args) {
+        BankasDati bankasDati = new BankasDati();
+        System.out.println(bankasDati.bankName);
+        System.out.println(bankasDati.bankName2);
+        System.out.println(bankasDati.cardNumber);
+        System.out.println(bankasDati.paymentProcessor);
+        System.out.println(bankasDati.secirityNumber);
+
+    }
+
+
+
+}
